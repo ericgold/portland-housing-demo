a3379069f649e1555fe5ccde3d4d92cce3954a21
ericgold <egold05@gmail.com>
2017-08-31 20:10:13 -0700

    add bar chart

--- src/Components/HousingChart.js
+++ src/Components/HousingChart.js
@@ -1,12 +1,17 @@
 import React from 'react';
-import { BarChart } from 'recharts';
+import { BarChart, XAxis, YAxis, Bar, Tooltip, Legend } from 'recharts';
 
 export default function HousingChart(props) {
-	const data = props.data;
+	//const data = props.data;
 	return (
 		<div>
 			<h2>Housing Chart</h2>
-			<BarChart width={600} height={400} data={data}>
+			<BarChart width={1200} height={400} data={props.data}>
+				<YAxis dataKey="rent_amt" />
+				<XAxis dataKey="neighborhood" />
+				<Tooltip />
+				<Legend />
+				<Bar dataKey="rent_amt" fill="#000000" />
 			</BarChart>
 		</div>
 	)
